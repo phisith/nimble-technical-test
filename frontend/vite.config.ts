78391cdd97f077261c95270a2bcd2833a7309c17
phisith <aham226@hotmail.com>
2023-02-24 22:37:55 +0700

    update vite.config.ts for setup docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: {
+    watch: { usePolling: true },
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
 });
