d7fb45056372ddea712c1eb41829da8947a7c576
phisith <aham226@hotmail.com>
2023-02-27 21:59:49 +0700

    setup express 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+import express, {Express, Request, Response} from "express";
+
+const app: Express = express()
+const PORT = process.env.BACKEND_PORT
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello Candy from Express TS");
+});
+
+app.listen(PORT, () => {
+    console.log(`now Listening on port ${PORT}`);
+});
+
